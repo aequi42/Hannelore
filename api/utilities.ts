5af421f14fa56de48d3b87b34d3c2469634c2fad
aequi42 <aequi42@users.noreply.github.com>
2020-04-03 12:17:57 +0200

    added weather

--- api/utilities.ts
+++ api/utilities.ts
@@ -11,6 +11,20 @@ type SendMessage = {
   reply_markup?: never; //TODO
 };
 
+export async function sendMarkupMessage(
+  message: string,
+  chat_id: string | number,
+  reply_id?: number
+) {
+  return handleMessage({
+    text: message,
+    chat_id,
+    reply_to_message_id: reply_id,
+    parse_mode: "HTML",
+    disable_notification: true
+  });
+}
+
 export async function sendMarkdownMessage(
   message: string,
   chat_id: string | number,
