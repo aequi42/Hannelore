6fdcd05de741baf4ac8c9d94c21879a24a7869c9
aequi42 <aequi42@users.noreply.github.com>
2021-12-04 22:47:09 +0100

    Improve Performance

--- messagehandlers/travelPicture.ts
+++ messagehandlers/travelPicture.ts
@@ -25,27 +25,39 @@ function canHandle(update: Update) {
   return update.message.text.indexOf("/travelpicture") == 0;
 }
 
+var cache = new Array<string>();
+
+async function getFromCacheOrLoad(client: any) {
+  if (cache.length) {
+    log("Debug", "CACHE HIT");
+    return [...cache];
+  }
+  log("Debug", "CACHE MISS");
+  const directoryItems: getDirectoryContentsResult =
+    await client.getDirectoryContents(`/files/${nextcloudUser}/Die4Lustigen5`, {
+      deep: true,
+      glob: "**/*.jpg",
+    });
+  console.timeLog("client");
+  cache = directoryItems.map(({ filename }) => filename);
+  setTimeout(() => (cache = new Array<string>()), 5 * 60 * 1000);
+  return cache;
+}
+
 async function handle(update: Update) {
+  console.time()
   const client = createClient(nextcloudUrl, {
     username: nextcloudUser,
     password: nextcloudPassword,
   });
-  const directoryItems: getDirectoryContentsResult =
-    await client.getDirectoryContents(
-      `/files/${nextcloudUser}/Die4Lustigen5`,
-      {
-        deep: true,
-        glob: "**/*.jpg",
-      }
-    );
-  log("Debug", JSON.stringify(directoryItems));
-  const correctSize = directoryItems.filter((i) => i.size < 10000000);
+  console.timeLog()
+  const correctSize = await getFromCacheOrLoad(client);
   const count = correctSize.length;
   const randomIdx = Math.floor(Math.random() * (count + 1));
-  const image = correctSize[randomIdx];
-  const { filename } = image;
+  const filename = correctSize[randomIdx];
+  console.timeLog()
   const contentBuffer = await client.getFileContents(filename);
-  log("Debug", contentBuffer);
+  console.timeEnd()
   return await sendPhoto(contentBuffer, update.message!.chat.id);
 }
 
